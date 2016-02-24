dd8173cdeee4770d7f838dc46ee6da391b8baba2
aj3x <ajrussell94@hotmail.com>
2016-02-23 21:26:16 -0600

    Intial Assets

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+    Rigidbody2D r_body;
+    float move_force;
+    float input_limit;
+	// Use this for initialization
+	void Start () {
+        r_body = this.GetComponent<Rigidbody2D>();
+        move_force = 10f;
+        input_limit = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = Input.acceleration.normalized;
+        
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) < input_limit) {
+            r_body.drag = 10;
+        } else {
+            r_body.drag = 1;
+            r_body.velocity = new Vector2(Input.GetAxis("Horizontal") * 2, 0);
+            //r_body.AddForce(new Vector2(Input.GetAxis("Horizontal") * move_force, 0));
+        }
+	}
+}
