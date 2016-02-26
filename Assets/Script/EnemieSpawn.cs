e18c0d25c4ce0b5e0a4531165f3473fb10eb0e32
aj3x <ajrussell94@hotmail.com>
2016-02-26 10:53:36 -0600

    Enemy SpawnerMove

--- Assets/Script/EnemieSpawn.cs
+++ Assets/Script/EnemieSpawn.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class EnemieSpawn : MonoBehaviour {
+    public GameObject enemy;
+    public float wait_time = 2f;
+    public float top_speed = 3;
+    public float border = 3f;
+    Rigidbody2D r_body;
 
 	// Use this for initialization
 	void Start () {
-	
+        Invoke("Spawn", wait_time);
+        r_body = GetComponent<Rigidbody2D>();
+        r_body.velocity = new Vector2(top_speed, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    void FixedUpdate() {
+        if(transform.position.x < -border)
+            r_body.velocity = (new Vector2(top_speed,0));
+        if(transform.position.x > border) {
+            r_body.velocity = (new Vector2(-top_speed, 0));
+        }
+    }
+
+
+    void Spawn() {
+        Instantiate<GameObject>(enemy);
+    }
 }
